a6741b3ad9759d04a3ee244e05fc91db52995855
Tazar Chriss <baltazarchristian49@gmail.com>
2024-05-31 01:36:06 +0300

    minor

--- resources/views/dashboards/collectiondashboard.blade.php
+++ resources/views/dashboards/collectiondashboard.blade.php
@@ -17,6 +17,30 @@
         <div class="bg-white pd-20 box-shadow border-radius-5 height-100-p">
             <div class="project-info clearfix">
                 <div class="project-info-left">
+                    <div class="icon box-shadow bg-light-orange text-white">
+                        <i class="fa fa-list-alt"></i>
+                    </div>
+                </div>
+                <div class="project-info-right">
+                    <span class="no text-light-orange weight-500 font-24">2 Tsh</span>
+                    <p class="weight-400 font-18">Pending</p>
+                </div>
+            </div>
+            <div class="project-info-progress">
+                <div class="row clearfix">
+                    <div class="col-sm-6 text-muted weight-500">Review</div>
+                    <div class="col-sm-6 text-right weight-500 font-14 text-muted">Good</div>
+                </div>
+                <div class="progress" style="height: 10px;">
+                    <div class="progress-bar bg-light-orange progress-bar-striped progress-bar-animated" role="progressbar" style="width: 80%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+        <div class="bg-white pd-20 box-shadow border-radius-5 height-100-p">
+            <div class="project-info clearfix">
+                <div class="project-info-left">
                     <div class="icon box-shadow bg-blue text-white">
                         <i class="fa fa-briefcase"></i>
                     </div>
@@ -61,30 +85,7 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
-        <div class="bg-white pd-20 box-shadow border-radius-5 height-100-p">
-            <div class="project-info clearfix">
-                <div class="project-info-left">
-                    <div class="icon box-shadow bg-light-orange text-white">
-                        <i class="fa fa-list-alt"></i>
-                    </div>
-                </div>
-                <div class="project-info-right">
-                    <span class="no text-light-orange weight-500 font-24">2 Tsh</span>
-                    <p class="weight-400 font-18">Pending</p>
-                </div>
-            </div>
-            <div class="project-info-progress">
-                <div class="row clearfix">
-                    <div class="col-sm-6 text-muted weight-500">Review</div>
-                    <div class="col-sm-6 text-right weight-500 font-14 text-muted">Good</div>
-                </div>
-                <div class="progress" style="height: 10px;">
-                    <div class="progress-bar bg-light-orange progress-bar-striped progress-bar-animated" role="progressbar" style="width: 80%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-            </div>
-        </div>
-    </div>
+
     <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
         <div class="bg-white pd-20 box-shadow border-radius-5 margin-5 height-100-p">
             <div class="project-info clearfix">
