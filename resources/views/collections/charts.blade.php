58d11c1cda1e3f62e84074e98bcce72ad6569308
Tazar Chriss <baltazarchristian49@gmail.com>
2024-05-31 02:56:05 +0300

    fixed

--- resources/views/collections/charts.blade.php
+++ resources/views/collections/charts.blade.php
@@ -55,7 +55,46 @@
     </div>
 
     <div class="col-md-6">
-  
+        <div class="">
+            <div class="bg-white pd-20 box-shadow border-radius-5 height-100-p">
+                <h4 class="mb-30">To Do list</h4>
+                <div class="to-do-list mx-h-450 ">
+                    <ul>
+                        <li>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                <label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck2">
+                                <label class="custom-control-label" for="customCheck2">Check this custom checkbox</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck3">
+                                <label class="custom-control-label" for="customCheck3">Check this custom checkbox</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck4">
+                                <label class="custom-control-label" for="customCheck4">Check this custom checkbox</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck5">
+                                <label class="custom-control-label" for="customCheck5">Check this custom checkbox</label>
+                            </div>
+                        </li>
+                      
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 
 </div>
