8f49dd09e582d51f4fada30a2bba2891cd5d9449
Tazar Chriss <baltazarchristian49@gmail.com>
2024-05-30 18:32:28 +0300

    mods

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,5 +9,27 @@ Dashboard
 @section('content')
 
 
+@php
+    $departmentName = '';
+    switch(Auth::user()->department_id) {
+        case 1:
+            $departmentName = "'dashboards.masterdashboard'";
+            break;
+        case 2:
+            $departmentName = 'Sales';
+            break;
+        case 3:
+            $departmentName = 'Collection';
+            break;
+        case 4:
+            $departmentName = 'Accountant';
+            break;
+    }
+@endphp
+
+@if($departmentName!='')
+    @include($departmentName);
+@endif
+
 
 @endsection
