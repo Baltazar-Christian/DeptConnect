55b082bee9c07c4a34b6d68328da4dee6b25765f
Tazar Chriss <baltazarchristian49@gmail.com>
2024-05-30 22:51:24 +0300

    changes

--- resources/views/dashboards/salesdashboard.blade.php
+++ resources/views/dashboards/salesdashboard.blade.php
@@ -5,7 +5,7 @@
             <div class="card-body d-flex flex-column">
                 <div class="d-flex justify-content-between mb-3">
                     <h5 class="card-title mb-0">
-                       Closed Sales
+                        Closed Sales
                     </h5>
 
                     <div class="card-title-sub">
@@ -14,7 +14,7 @@
                 </div>
 
                 <div class="progress mt-auto">
-                    <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0"
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0"
                         aria-valuemax="100">3/4</div>
                 </div>
             </div>
@@ -76,7 +76,77 @@
 </div>
 {{-- ./ --}}
 
+<div>
+    <div class="card mb-grid ">
+        <div class="card-header">
+            <ul class="nav nav-pills card-header-pills" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="card-tab-1" data-toggle="tab" href="#card-tab-content-1"
+                        role="tab" aria-controls="card-tab-content-1" aria-selected="true">Prospects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="card-tab-2" data-toggle="tab" href="#card-tab-content-2" role="tab"
+                        aria-controls="card-tab-1" aria-selected="false">Customers</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="card-tab-2" data-toggle="tab" href="#card-tab-content-5" role="tab"
+                        aria-controls="card-tab-1" aria-selected="false">Follow Ups</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="card-tab-2" data-toggle="tab" href="#card-tab-content-4" role="tab"
+                        aria-controls="card-tab-1" aria-selected="false">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="card-tab-2" data-toggle="tab" href="#card-tab-content-3"
+                        role="tab" aria-controls="card-tab-2" aria-selected="false">Reports</a>
+                </li>
+            </ul>
+        </div>
+        <div class="card-body">
+            <div class="tab-content">
+                {{-- For Prospects --}}
+                <div class="tab-pane fade show active" id="card-tab-content-1" role="tabpanel"
+                    aria-labelledby="card-tab-1">
+                    {{-- Start of Prospects --}}
+                    @include('sales.prospects')
+                    {{-- ./ --}}
+                </div>
+                {{-- ./ --}}
 
-{{-- Start of Prospects --}}
-@include('sales.prospects')
-{{-- ./ --}}
+                {{-- For Customers --}}
+                <div class="tab-pane fade" id="card-tab-content-2" role="tabpanel" aria-labelledby="card-tab-2">
+                    <h4 class="card-title">Customers</h4>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                {{-- For Reports --}}
+
+                {{-- For products --}}
+                <div class="tab-pane fade" id="card-tab-content-4" role="tabpanel" aria-labelledby="card-tab-2">
+                    <h4 class="card-title">Products</h4>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                {{-- ./ --}}
+
+
+                {{-- For Follow ups --}}
+                <div class="tab-pane fade" id="card-tab-content-5" role="tabpanel" aria-labelledby="card-tab-2">
+                    <h4 class="card-title">Follow Ups</h4>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+
+                {{-- ./ --}}
+
+
+                <div class="tab-pane fade" id="card-tab-content-3" role="tabpanel" aria-labelledby="card-tab-3">
+                    <h4 class="card-title">Special title treatment</h4>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
