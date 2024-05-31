fd22d885e31102525b1163809d4a45ece6b99e29
Tazar Chriss <baltazarchristian49@gmail.com>
2024-05-31 12:08:26 +0300

    fixed

--- resources/views/sales/products.blade.php
+++ resources/views/sales/products.blade.php
@@ -32,7 +32,7 @@
 
    <!-- Product Modal -->
 <div class="modal fade" id="productModal" tabindex="-1" role="dialog" aria-labelledby="productModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg  modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="productModalLabel">Add New Product</h5>
