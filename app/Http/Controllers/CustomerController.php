bb2dd9f8a7013297a4644c3c1f60d48269994168
Tazar Chriss <baltazarchristian49@gmail.com>
2024-05-31 10:56:49 +0300

    fixed

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -7,32 +7,51 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    public function index()
-    {
-        $customers = Customer::all();
-        return response()->json($customers);
-    }
+  // Get all customers
+  public function index()
+  {
+      $customers = Customer::all();
+      return response()->json($customers);
+  }
 
-    public function store(Request $request)
-    {
-        $customer = Customer::create($request->all());
-        return response()->json($customer, 201);
-    }
+  // Store a new customer
+  public function store(Request $request)
+  {
+      $validatedData = $request->validate([
+          'name' => 'required|max:255',
+          'email' => 'required|email|unique:customers,email',
+          'phone' => 'required|numeric',
+          'address' => 'required'
+      ]);
 
-    public function show(Customer $customer)
-    {
-        return response()->json($customer);
-    }
+      $customer = Customer::create($validatedData);
+      return response()->json($customer, 201);
+  }
 
-    public function update(Request $request, Customer $customer)
-    {
-        $customer->update($request->all());
-        return response()->json($customer);
-    }
+  // Show a single customer
+  public function show(Customer $customer)
+  {
+      return response()->json($customer);
+  }
 
-    public function destroy(Customer $customer)
-    {
-        $customer->delete();
-        return response()->json(null, 204);
-    }
+  // Update an existing customer
+  public function update(Request $request, Customer $customer)
+  {
+      $validatedData = $request->validate([
+          'name' => 'sometimes|required|max:255',
+          'email' => 'sometimes|required|email|unique:customers,email,' . $customer->id,
+          'phone' => 'sometimes|required|numeric',
+          'address' => 'sometimes|required'
+      ]);
+
+      $customer->update($validatedData);
+      return response()->json($customer);
+  }
+
+  // Delete a customer
+  public function destroy(Customer $customer)
+  {
+      $customer->delete();
+      return response()->json(null, 204);
+  }
 }
